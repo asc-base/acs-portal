aa5b7f78d7daf295dbc0c193b8387c1db7bec8b8
ZM <zmxidn@gmail.com>
2025-06-24 11:27:35 +0700

    Create Page admin NewsManagement

---
+++ src/core/models/news.ts
@@ -0,0 +1,80 @@
+import { News } from "@/interface/news";
+const BASE_URL = "https://jsonplaceholder.typicode.com";
+
+
+export const getNews = async (): Promise<News[] | Error> => {
+  const response = await fetch(`${BASE_URL}/news`);
+  
+  if (!response.ok) {
+    return new Error("Failed to fetch all news");
+  }
+
+  const data: News[] = await response.json();
+  return data;
+};
+
+
+export const getNewsById = async (id: string): Promise<News| Error> => {
+  const response = await fetch(`${BASE_URL}/news/${id}`);
+  if (!response.ok) {
+      return new Error("Failed to fetch example data");
+    }
+    const data: News = await response.json();
+    return data;
+};
+
+export const postNews = async (data: News): Promise<true | Error> => {
+  const response = await fetch(`${BASE_URL}/news`, {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify(data),
+  });
+
+  if (!response.ok) {
+    return new Error(`Failed to post news: ${response.status} ${response.statusText}`);
+  }
+  return true; 
+};
+
+
+export const patchNews = async (
+  id: string,
+  data: Partial<News>
+): Promise<true | Error> => {
+  const response = await fetch(`${BASE_URL}/news/${id}`, {
+    method: 'PATCH',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify(data),
+  });
+
+  if (!response.ok) {
+    return new Error(`Failed to patch news: ${response.status} ${response.statusText}`);
+  }
+
+  return true;
+};
+
+
+export const deleteNews = async (id: string): Promise<true | Error> => {
+  const response = await fetch(`${BASE_URL}/news/${id}`, {
+    method: 'DELETE',
+  });
+
+  if (!response.ok) {
+    return new Error(`Failed to delete news: ${response.status} ${response.statusText}`);
+  }
+
+  return true;
+};
+
+
+export const getAllNews = async (): Promise<News[] | Error> => {
+  const response = await fetch(`${BASE_URL}/news`);
+  
+  if (!response.ok) {
+    return new Error("Failed to fetch all news");
+  }
+
+  const data: News[] = await response.json();
+  return data;
+};
---
+++ src/interface/news.ts
@@ -0,0 +1,9 @@
+export interface News {
+  id?: string;
+  title: string;
+  categoryId: number;
+  startDate: string;
+  endDate: string;
+  detail: string;
+  image?: string;
+}
