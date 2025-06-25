179f819074a6290f623408120b305bb82f7f1627
ZM <zmxidn@gmail.com>
2025-06-26 00:36:11 +0700

    Create Page AdminLogin

--- src/app/admin/layout.tsx
+++ src/app/admin/layout.tsx
@@ -1,13 +1,19 @@
 "use client";
 import { ReactNode } from "react";
 import { EdgeSidebarAdmin } from "@/components/edgesidebaradmin";
+import { usePathname } from "next/navigation";
 
 const Layout = ({ children }: Readonly<{ children: ReactNode }>) => {
+  const pathname = usePathname();
+  const hideSidebar = pathname === "/admin/login";
+  
   return (
     <div className="jun-layout flex h-screen">
-      <aside className="jun-sidebar w-64">
-        <EdgeSidebarAdmin username="Admin01"/>
-      </aside>
+      {!hideSidebar && (
+        <aside className="jun-sidebar w-64">
+          <EdgeSidebarAdmin username="Admin01" />
+        </aside>
+      )}
         <main className="jun-content flex-1 overflow-y-auto">
           {children}
         </main>
