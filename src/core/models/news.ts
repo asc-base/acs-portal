e27c3e944664fe7766ed026f988dedae98468861
V0yager2 <charnatip15@gmail.com>
2025-07-13 21:17:26 +0700

    feat : create deleteNews model

--- src/core/models/news.ts
+++ src/core/models/news.ts
@@ -12,3 +12,21 @@ export const getNews = async (page: number , pageSize : number): Promise<News[] 
   const data = await response.json();
   return data.data;
 };
+
+export const deleteNews = async (id: string, token: string): Promise<News | Error> => {
+  const response = await fetch(`${API_URL}/news/${id}`, {
+    method: "DELETE",
+    headers: {
+      "Content-Type": "application/json",
+      "Authorixation": `Bearer ${token}`
+    },
+  }
+  )
+
+  if (!response.ok) {
+    return new Error("Failed to delete news");
+  }
+
+  const data = await response.json();
+  return data.data;
+}
