0c5aa329e5b9928f4c3edd5a9b6cf282c0ae7c3f
SukumJS <sukumpat2547@gmail.com>
2025-07-21 20:33:37 +0700

    added Authorization in header

--- src/core/models/course.ts
+++ src/core/models/course.ts
@@ -1,11 +1,12 @@
 import { API_URL } from "@/config/config";
 import { Course } from "@/interface/course";
 
-export const createCourse = async (course: Course): Promise<Course | Error> => {
+export const createCourse = async (course: Course, token: string): Promise<Course | Error> => {
   const response = await fetch(`${API_URL}/course`, {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
+      "Authorization": `Bearer ${token}`,
     },
     body: JSON.stringify(course),
   });
