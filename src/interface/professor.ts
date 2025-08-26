6b73b9ec69e4098cbeca3d07209392e10c1cc119
Sukum <sukumpat2547@gmail.com>
2025-08-26 23:55:13 +0700

    Added Professor Interface

---
+++ src/interface/professor.ts
@@ -0,0 +1,29 @@
+export interface IProfessor {
+    id: number;
+    firstNameTh: string;
+    lastNameTh: string;
+    firstNameEn: string;
+    lastNameEn: string;
+    email: string;
+    majorPositionId: number;
+    academecpositionId: number;
+    profRoom: string;
+    IsPassword: boolean;
+    expertFeild: string[];
+    education: string[];
+    image: string
+}
+
+export interface ICreateNews {
+    firstNameTh: string;
+    lastNameTh: string;
+    firstNameEn: string;
+    lastNameEn: string;
+    email: string;
+    majorPositionId: number;
+    academecpositionId: number;
+    profRoom: string;
+    IsPassword: boolean;
+    expertFeild: string[];
+    education: string[];
+}
