9e9227bae57559c22ecb8508c8934e80a259324f
ZM <zmxidn@gmail.com>
2025-09-16 22:07:41 +0700

    refactor: adjust interface ProfessorCard

--- src/interface/user.ts
+++ src/interface/user.ts
@@ -2,3 +2,14 @@ export interface InputLogin {
   email: string;
   password: string;
 }
+
+export interface User {
+  id: number;
+  email: string;
+  firstNameTh: string;
+  lastNameTh: string;
+  firstNameEn: string;
+  lastNameEn: string;
+  nickName?: string;
+  imageUrl: string;
+}
