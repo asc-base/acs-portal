7599bc3539308c1fab7771dc8db1217830867ccc
ZM <zmxidn@gmail.com>
2025-09-16 18:12:49 +0700

    feat: add getProfessor Repository

--- src/core/ports/professor.repository.ts
+++ src/core/ports/professor.repository.ts
@@ -1,6 +1,10 @@
-import { ApiResponse } from "@/interface/response";
+import { ApiResponse, Pageable } from "@/interface/response";
 import { IProfessor } from "../domain/professor";
 
 export interface IProfessorRepository {
+     getProfessors(
+        page: number,
+        pageSize: number,
+      ): Promise<ApiResponse<Pageable<IProfessor>>>;
     getProfessorById(id: string): Promise<ApiResponse<IProfessor>>;
 }
