c6d73b058e8f0543c894a701fc7232c7db51ee78
V0yager2 <charnatip15@gmail.com>
2025-09-18 17:09:24 +0700

    change : delete unuse file from curriculum and typecourse service

--- src/core/viewmodels/curriculum.ts
+++ src/core/viewmodels/curriculum.ts
@@ -1,19 +1,7 @@
-import { CreateCurriculumModel, GetCurriculum } from "../models/curriculum";
+import { CreateCurriculumModel} from "../models/curriculum";
 import { Curriculum } from "@/interface/curriculum";
 
 export const CreateCurriculum = async (data: Curriculum, token: string) => {
   const result = await CreateCurriculumModel(data, token);
   return result;
 };
-
-export const fetchCurriculum = async () => {
-  try {
-          const result = await GetCurriculum();
-          if (result instanceof Error) throw result;
-          return result;
-      }
-      catch (error) {
-          console.error("Error fetching curriculum data:", error);
-          return [];
-      }
-}
