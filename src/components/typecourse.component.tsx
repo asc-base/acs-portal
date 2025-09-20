0eb80690494dfcbd6a623e1f2978b913abda8fcc
V0yager2 <charnatip15@gmail.com>
2025-09-20 22:46:35 +0700

    feat : make typecourse component  point to next page

--- src/components/typecourse.component.tsx
+++ src/components/typecourse.component.tsx
@@ -12,12 +12,14 @@ const typeCourseImage = [
 ];
 
 interface TypeCourseProps {
+  curriculumId:number;
+  typeCourseId:number
   name: string;
   description: string;
   index: number;
 }
 
-export const TypeCourseComponent: FC<TypeCourseProps> = ({ name, description, index }) => {
+export const TypeCourseComponent: FC<TypeCourseProps> = ({curriculumId, typeCourseId, name, description, index }) => {
   return (
     <div className="flex flex-col items-center justify-center text-center mb-10">
       <div className="w-[120px] h-[120px]">
@@ -36,7 +38,7 @@ export const TypeCourseComponent: FC<TypeCourseProps> = ({ name, description, in
         rel="noopener noreferrer"
         className="border-primary03 text-primary03"
       >
-        <Link href="#" target="_blank">
+        <Link href={`/course?page=1&pageSize=12&prerequisite=true&curriculumId=${curriculumId}&typeCourseId=${typeCourseId}&typeCourseName=${name}`}>
           ดูรายวิชา
         </Link>
       </Button>
