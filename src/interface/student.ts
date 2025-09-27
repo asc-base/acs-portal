fdd7ed6e26c57c5b04827fc246b79d5f9c7fae98
ZM <zmxidn@gmail.com>
2025-09-28 01:36:15 +0700

    feat: add Student and Project interfaces in src/interface and domain

---
+++ src/interface/student.ts
@@ -0,0 +1,17 @@
+import { IUser } from "./user";
+import { IProject } from "./project";
+
+export interface IStudent {
+  id: number;
+  studentId: string;
+  user: IUser;
+  description: string;
+  yearOfFirstAdmission: string;
+  linkedin: string;
+  facebook: string;
+  instagram: string;
+  github: string;
+  classOf: string;
+  projects: IProject[];
+}
+
