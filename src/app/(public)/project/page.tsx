cb9e230b7a8eaa51fc4781840848e1072b1b24bf
ParkPawapon <pawaponthammalangka@outlook.com>
2025-09-29 11:57:42 +0700

    feat(PROJECT-003): add project folder with student project page and projectcard.png

---
+++ src/app/(public)/project/page.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import ProjectCardLandingPage from "./projectcard.landingpage";
+const Page = async () => {
+
+  return (
+    <div className="container mx-auto px-0 md:px-16 lg:px-0 py-5">
+      <ProjectCardLandingPage />
+    </div>
+  );
+};
+
+export default Page;
