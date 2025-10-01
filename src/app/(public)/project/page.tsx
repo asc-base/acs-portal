5896e5db69b5616a601e3b45fa721fc7b57cccf3
Pawapon Thammalangka <108130529+ParkPawapon@users.noreply.github.com>
2025-10-01 19:52:29 +0700

    Update page.tsx

--- src/app/(public)/project/page.tsx
+++ src/app/(public)/project/page.tsx
@@ -1,10 +1,10 @@
 import React from "react";
-import ProjectCard from "./project.list.components";
-const Page = async () => {
+import ProjectList from "./project.list.components";
 
+const Page = async () => {
   return (
     <div className="container mx-auto px-0 md:px-16 lg:px-0 py-5">
-      <ProjectCard />
+      <ProjectList />
     </div>
   );
 };
