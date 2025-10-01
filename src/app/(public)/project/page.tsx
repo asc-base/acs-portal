d9bc296b538af3a282f846252e827060446739eb
Pawapon Thammalangka <108130529+ParkPawapon@users.noreply.github.com>
2025-10-01 19:33:16 +0700

    Update page.tsx

--- src/app/(public)/project/page.tsx
+++ src/app/(public)/project/page.tsx
@@ -1,10 +1,10 @@
 import React from "react";
-import ProjectCardLandingPage from "./projectcard";
+import ProjectCard from "./projectcard";
 const Page = async () => {
 
   return (
     <div className="container mx-auto px-0 md:px-16 lg:px-0 py-5">
-      <ProjectCardLandingPage />
+      <ProjectCard />
     </div>
   );
 };
