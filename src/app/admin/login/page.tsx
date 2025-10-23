220ecc85d224950dcb1e54e2103bf6c835977c6a
V0yager <charnatip15@gmail.com>
2025-10-23 15:47:08 +0700

    feat: craete admin login page

---
+++ src/app/admin/login/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import AdminLoginLandingPage from "./admin.login.landingpage";
+
+export default function Page() {
+  // ไม่มีการเรียกหลังบ้าน ใส่ UI-only
+  return (
+    <div className="w-full">
+      <AdminLoginLandingPage />
+    </div>
+  );
+}
