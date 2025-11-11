15a92f737714292c2160c270900773216d1c1a84
ZM <zmxidn@gmail.com>
2025-11-12 00:14:30 +0700

    refactor: show clear icon only when the search field has input

--- src/app/admin/students/students.landingpage.tsx
+++ src/app/admin/students/students.landingpage.tsx
@@ -99,13 +99,15 @@ const StudentsLandingpage = ({
               {...register("search")}
               className="border-neutral04 text-h4 h-[44px] w-[280px] rounded-sm border pl-10"
             />
-            <button
-              type="button"
-              onClick={() => reset()}
-              className="absolute top-1/2 right-2 -translate-y-1/2 text-gray-500 hover:text-gray-700"
-            >
-              <CloseIcon fontSize="small" />
-            </button>
+           {watch("search") && (
+              <button
+                type="button"
+                onClick={() => reset({ search: "" })}
+                className="absolute right-2 top-1/2 -translate-y-1/2 text-gray-500 hover:text-gray-700"
+              >
+                <CloseIcon fontSize="small" />
+              </button>
+            )}
           </form>
         </div>
       </div>
