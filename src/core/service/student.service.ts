f30ada10b50a008fe948f53ac482d8d386a3c058
V0yager <charnatip15@gmail.com>
2026-01-22 22:24:39 +0700

    feat : craete updatestudent service

--- src/core/service/student.service.ts
+++ src/core/service/student.service.ts
@@ -1,5 +1,5 @@
 import { IStudentRepository } from "../ports/student.repository";
-import { IStudent, QueryStudent , ICreateStudent} from "../domain/student";
+import { IStudent, QueryStudent , ICreateStudent , IUpdateStudent} from "../domain/student";
 import { Pageable } from "@/interface/response";
 
 export class StudentService {
@@ -18,5 +18,24 @@ export class StudentService {
   async createStudent(data : ICreateStudent[] , classBookId : number): Promise<IStudent[]> {
         const response = await this.studentRepository.createStudent(data ,classBookId)
         return response.data;
+  }
+
+  async updateStudent(data : IUpdateStudent, image:File | null , classBookId : number , studentId : number): Promise<IStudent | null> {
+    try{
+       const formData = new FormData();
+      Object.entries(data).forEach(([key, value]) => {
+        formData.append(key, value?.toString() ?? "");
+      });
+      if(image)formData.append("image", image);
+      formData.append("classBookId",classBookId.toString());
+
+      const response = await this.studentRepository.updateStudent(formData ,studentId)
+      return response.data;
+    }catch (error) {
+      console.error("Failed to update student:", error);
+      return null;
+    }
+
+        
   }
 }
