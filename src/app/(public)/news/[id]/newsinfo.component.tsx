c4cc3de535f4b34494af45aea2061f6d9b3326fc
V0yager <charnatip15@gmail.com>
2026-05-10 01:24:08 +0700

    fix : justify recomend news center

--- src/app/(public)/news/[id]/newsinfo.component.tsx
+++ src/app/(public)/news/[id]/newsinfo.component.tsx
@@ -30,7 +30,7 @@ const NewsInfoComponent = ({ newsInfo, recommendNews }: NewsInfoProps) => {
         {newsInfo.title && <span>{newsInfo.title}</span>}
       </Breadcrumbs>
       <div>
-        <div className="relative aspect-video w-full h-[560px] overflow-hidden rounded-xl">
+        <div className="relative aspect-video h-[560px] w-full overflow-hidden rounded-xl">
           <Skeleton
             data-skeleton
             variant="rectangular"
@@ -69,11 +69,14 @@ const NewsInfoComponent = ({ newsInfo, recommendNews }: NewsInfoProps) => {
         <h3 className="text-accent04 font-bold lg:text-2xl">
           ข่าวที่น่าสนใจอื่นๆ
         </h3>
+
         <div className="grid grid-cols-1 gap-8 md:grid-cols-2 lg:grid-cols-3">
           {recommendNews?.map((news) => (
-            <Link key={news.id} href={`/news/${news.id}`} className="block w-full">
-              <NewsCard news={news} />
-            </Link>
+            <div key={news.id} className="flex justify-center">
+              <Link href={`/news/${news.id}`} className="block w-full max-w-sm">
+                <NewsCard news={news} />
+              </Link>
+            </div>
           ))}
         </div>
       </div>
