f48e24ffa8b9d1e5206c74c70523f8c7169ab311
orporpanggg <ppc.opal@gmail.com>
2026-05-16 20:57:22 +0700

    fix: update EdgeSidebarAdmin to display user's full name dynamically

--- src/app/admin/layout.tsx
+++ src/app/admin/layout.tsx
@@ -2,16 +2,20 @@
 import { ReactNode } from "react";
 import { EdgeSidebarAdmin } from "@/components/edgesidebaradmin";
 import { usePathname } from "next/navigation";
+import { useAuthStore } from "@/store/auth";
 
 const Layout = ({ children }: Readonly<{ children: ReactNode }>) => {
   const pathname = usePathname();
   const hideSidebar = pathname === "/admin/auth";
+
+  const user = useAuthStore((state) => state.user);
+  console.log("ข้อมูล User ใน Store คือ:", user);
   
   return (
     <div className="jun-layout flex h-screen">
       {!hideSidebar && (
         <aside className="jun-sidebar w-64">
-          <EdgeSidebarAdmin username="Admin01" />
+          <EdgeSidebarAdmin username={user ? `${user.firstNameTh} ${user.lastNameTh}` : "Admin"} />
         </aside>
       )}
         <main className="jun-content flex-1 overflow-y-auto">
