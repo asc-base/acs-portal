f4a2f769363d96fe577985e3b7422026dc88ebb1
😺 <asnaweeezor@gmail.com>
2026-06-21 12:44:03 +0700

    feat: implement authentication repository and student profile management page

--- src/app/(public)/profile/page.tsx
+++ src/app/(public)/profile/page.tsx
@@ -1,10 +1,20 @@
-import React from "react";
 import ProfileForm from "./profileform";
+import { authService, studentService } from "@/infra/container";
+import { cookies } from "next/headers";
+import { redirect } from "next/navigation";
 
 const page = async () => {
-  return (
-      <ProfileForm />
-  );
+  const cookiesStore = await cookies();
+  const token = cookiesStore.get("accessToken")?.value;
+  if (!token) {
+    redirect("/auth/student");
+  }
+
+  const user = await authService.getUserData(token);
+  console.log(user);
+  const student = await studentService.getStudentByUserId(user.id);
+  console.log(student);
+  return <ProfileForm student={student} />;
 };
 
 export default page;
