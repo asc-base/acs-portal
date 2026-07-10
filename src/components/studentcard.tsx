ce405e17b4efe78d20e471194c0c89799a7d0431
😺 <asnaweeezor@gmail.com>
2026-07-10 08:11:18 +0700

    refactor: update student card styling (ACSWEBSITE-57)

--- src/components/studentcard.tsx
+++ src/components/studentcard.tsx
@@ -1,16 +1,5 @@
-import React, { FC } from "react";
-import {
-  Card,
-  CardContent,
-  CardMedia,
-  Typography,
-  Box,
-  IconButton,
-} from "@mui/material";
-import FacebookIcon from "@mui/icons-material/Facebook";
-import LinkedInIcon from "@mui/icons-material/LinkedIn";
-import InstagramIcon from "@mui/icons-material/Instagram";
-import GitHubIcon from "@mui/icons-material/GitHub";
+import { FC } from "react";
+import { Card, CardContent, CardMedia, Typography, Box } from "@mui/material";
 import AccountCircleIcon from "@mui/icons-material/AccountCircle";
 import { IStudent } from "@/core/domain/student";
 
@@ -46,8 +35,8 @@ export const StudentCard: FC<IStudent> = (props) => {
           />
         </Box>
       )}
-      <CardContent className="flex flex-1 flex-col items-center justify-center gap-2 text-center">
-        <Typography className="!text-h4 lg:!text-h3 !text-primary01 text-center !font-bold">
+      <CardContent className="flex flex-1 flex-col justify-center gap-1 p-3 !pb-3 text-left lg:p-4 lg:!pb-4">
+        <Typography className="!text-h2 !text-primary01 text-left !font-bold">
           <span className="lg:hidden">
             {`${props.user.firstNameTh} ${props.user.lastNameTh}`.length >=
             16 ? (
@@ -64,60 +53,13 @@ export const StudentCard: FC<IStudent> = (props) => {
             {props.user.firstNameTh} {props.user.lastNameTh}
           </span>
         </Typography>
-        <Box className="flex w-full flex-col items-center justify-between xl:mt-2 xl:flex-row">
-          <Typography className="!text-h5 lg:!text-h4 !text-primary01">
+        <Box className="mt-1 flex w-full flex-row items-center justify-between">
+          <Typography className="!text-h4 !text-neutral05">
+            รุ่นที่ {props.classBookID}
+          </Typography>
+          <Typography className="!text-h4 !text-neutral05">
             {`${props.studentCode.slice(0, 2)}-${props.studentCode.slice(-3)}`}
           </Typography>
-          <Box className="mt-2 flex md:m-0">
-            {props.facebook && (
-              <IconButton
-                component="a"
-                href={props.facebook}
-                target="_blank"
-                rel="noopener noreferrer"
-                sx={{ p: "2px", color: "var(--color-neutral05)" }}
-                onClick={(e) => e.stopPropagation()}
-              >
-                <FacebookIcon fontSize="small" />
-              </IconButton>
-            )}
-            {props.linkedin && (
-              <IconButton
-                component="a"
-                href={props.linkedin}
-                target="_blank"
-                rel="noopener noreferrer"
-                sx={{ p: "2px", color: "var(--color-neutral05)" }}
-                onClick={(e) => e.stopPropagation()}
-              >
-                <LinkedInIcon fontSize="small" />
-              </IconButton>
-            )}
-            {props.instagram && (
-              <IconButton
-                component="a"
-                href={props.instagram}
-                target="_blank"
-                rel="noopener noreferrer"
-                sx={{ p: "2px", color: "var(--color-neutral05)" }}
-                onClick={(e) => e.stopPropagation()}
-              >
-                <InstagramIcon fontSize="small" />
-              </IconButton>
-            )}
-            {props.github && (
-              <IconButton
-                component="a"
-                href={props.github}
-                target="_blank"
-                rel="noopener noreferrer"
-                sx={{ p: "2px", color: "var(--color-neutral05)" }}
-                onClick={(e) => e.stopPropagation()}
-              >
-                <GitHubIcon fontSize="small" />
-              </IconButton>
-            )}
-          </Box>
         </Box>
       </CardContent>
     </Card>
