7d3a5833c804368b0d938e8acede1b57d1169ed2
Sirawit Boonset <kongsirawit.bs@gmail.com>
2026-07-18 14:26:07 +0700

    feat: implement update project service

--- src/core/service/project.service.ts
+++ src/core/service/project.service.ts
@@ -1,5 +1,5 @@
 import { Pageable } from "@/interface/response";
-import { IProject, QueryProject, ICreateProject } from "../domain/project";
+import { IProject, QueryProject, ICreateProject, IUpdateProject } from "../domain/project";
 import { IProjectRepository } from "../ports/project.repository";
 
 export class ProjectService {
@@ -38,6 +38,29 @@ export class ProjectService {
 
   async deleteProject(id: number): Promise<IProject> {
     const response = await this.projectRepository.deleteProject(id);
+    return response.data;
+  }
+
+  async updateProject(id: string, payload: IUpdateProject, files?: { thumbnailFile?: File | null; assets?: File[] }): Promise<IProject> {
+    const formData = new FormData();
+
+    Object.entries(payload).forEach(([key, value]) => {
+      if (Array.isArray(value) || typeof value === 'object') {
+        formData.append(key, JSON.stringify(value));
+      } else {
+        formData.append(key, value?.toString() ?? "");
+      }
+    });
+
+    if (files?.thumbnailFile) {
+      formData.append("thumbnailFile", files.thumbnailFile);
+    }
+    
+    if (files?.assets) {
+      files.assets.forEach((file) => formData.append("assets", file));
+    }
+
+    const response = await this.projectRepository.updateProject(id, formData);
     return response.data;
   }
 }
