df5ecf1974770cff9a10b1c278ce7ed4e72a11d1
Sirawit Boonset <kongsirawit.bs@gmail.com>
2026-07-23 00:10:53 +0700

    fix: change required mark color in RHF components to red(E8360E)
    - change to red (E8360E)
    - write on tailwind.css style

--- src/components/form/RHFDatePicker.tsx
+++ src/components/form/RHFDatePicker.tsx
@@ -39,7 +39,7 @@ export function RHFDatePickerDayjs<T extends FieldValues>({
           {label && (
             <FormLabel>
               {label}
-              {(requiredMark || required) && <span style={{ color: "#FF383C", marginLeft: "4px" }}>*</span>}
+              {(requiredMark || required) && <span className="text-[#E8360E] ml-1">*</span>}
             </FormLabel>
           )}
 
