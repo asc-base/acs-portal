51402df6587dcec9400a2db2a790ba5a1699f660
KongXrZ <kongsirawit.bs@gmail.com>
2026-07-31 14:50:41 +0700

    fix: add fallback empty string for optional english names

--- src/app/admin/students/create/create.student.form.tsx
+++ src/app/admin/students/create/create.student.form.tsx
@@ -108,8 +108,8 @@ export const CreateStudentForm: FC<FormProfessorsProps> = ({
       const payload: ICreateStudent = {
         firstNameTh: data.firstNameTh,
         lastNameTh: data.lastNameTh,
-        firstNameEn: data.firstNameEn,
-        lastNameEn: data.lastNameEn,
+        firstNameEn: data.firstNameEn ?? "",
+        lastNameEn: data.lastNameEn ?? "",
         studentCode: data.studentCode,
         nickName: data.nickName,
         email: data.email,
