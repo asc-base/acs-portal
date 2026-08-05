c50975e895898cad2dee7ad327876ceffe00ac84
orporpanggg <ppc.opal@gmail.com>
2026-08-05 19:11:26 +0700

    feat: update NewsInformationForm to follow Figma and update schema for improved validation and type consistency

--- src/core/schema/news.ts
+++ src/core/schema/news.ts
@@ -31,23 +31,17 @@ export const UpdateNewsSchema = z.object({
   highlight: z.union([z.string().trim().min(1), z.file()]),
 });
 
-export const createNewsInformationSchema = (type: string) =>
+export const CreateNewsInformationSchema = (type: string) =>
   z
     .object({
       thumbnail: z.instanceof(File, { message: "กรุณาอัปโหลดรูปภาพ" }),
-      highlight: z.instanceof(File).optional(),
-      newsID: z.number().min(1, "กรุณาเลือกข่าว"),
-    })
-    .superRefine((data, ctx) => {
-      if (type === "highlight" && !data.highlight) {
-        ctx.addIssue({
-          code: "custom",
-          message: "กรุณาอัปโหลดรูปภาพไฮไลต์",
-          path: ["highlight"],
-        });
-      }
+      highlight:
+      type === "newshighlight"
+        ? z.instanceof(File, { message: "กรุณาอัปโหลดรูปภาพ" })
+        : z.instanceof(File).optional(),
+    newsID: z.number().min(1, "กรุณาเลือกข่าว"),
     });
 
 export type CreateNewsInputs = z.infer<typeof CreateNewsSchema>;
 export type UpdateNewsInputs = z.infer<typeof UpdateNewsSchema>;
-export type NewsInformationInputs = z.infer<ReturnType<typeof createNewsInformationSchema>>
+export type CreateNewsInformationInputs = z.infer<ReturnType<typeof CreateNewsInformationSchema>>
