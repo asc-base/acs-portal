f35880bd52a6977e94e5bc2594222483a3d13fb7
IKeeN-0 <hengkung20@gmail.com>
2026-09-14 11:00:48 +0700

    display student prefix in public profile
    user.ts
    - เพิ่มฟิลด์ shortNameTh?: string; และ shortNameEn?: string; ใน interface UserPrefix เพื่อรองรับ Type ของชื่อย่อคำนำหน้าทั้งไทยและอังกฤษ
    
    profileform.tsx
    - เพิ่มการแสดงผลคำนำหน้าชื่อในส่วนข้อมูลส่วนตัว
    - student.user.prefix?.nameTh แสดงคำนำหน้าชื่อภาษาไทย
    - student.user.prefix?.shortNameEn แสดงคำนำหน้าชื่อภาษาอังกฤษ

--- src/app/(public)/profile/profileform.tsx
+++ src/app/(public)/profile/profileform.tsx
@@ -318,7 +318,7 @@ const ProfileForm = ({ apiBase }: { apiBase: string }) => {
               <div className="text-center">:</div>
               <div className="text-primary01 font-bold">
                 {student?.user
-                  ? `${firstNameTh} ${lastNameTh}`.trim()
+                  ? `${student.user.prefix?.nameTh || ""} ${firstNameTh} ${lastNameTh}`.trim()
                   : "สมชาย ใจดี"}
               </div>
 
@@ -327,7 +327,7 @@ const ProfileForm = ({ apiBase }: { apiBase: string }) => {
               <div className="text-center">:</div>
               <div className="text-primary01 font-bold">
                 {student?.user
-                  ? `${firstNameEn} ${lastNameEn}`.trim()
+                  ? `${student.user.prefix?.shortNameEn || ""} ${firstNameEn} ${lastNameEn}`.trim()
                   : "Somchai Jaidee"}
               </div>
             </div>
